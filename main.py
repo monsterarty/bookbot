97a2866d4e271d54294d19619953332dab3b03b4
monsterarty <masleanco@gmail.com>
2024-11-16 12:09:02 +0100

    00002: main.py added read of book file, added count of words in book file

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+# Read frankenstein project First proj
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    print(text)
+    print("///")
+    print(f"{num_words} words found in the document")
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+        
+main()
+    
+
+
