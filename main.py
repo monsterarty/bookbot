5c1aa76ebded60569c113037e1ad77d8b958c6a8
monsterarty <masleanco@gmail.com>
2025-04-22 13:55:37 +0200

    3: Added printing report with sorted chars

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from stats import word_count
+from stats import word_count, char_count, char_count_sorted
+
+book_path = "./books/frankenstein.txt"
+
 
 def get_book_text():
 
-    with open("./books/frankenstein.txt", "r") as f:
+    with open(book_path, "r") as f:
         file_content = f.read()
         return file_content
     
-
-
-print(f"{word_count(get_book_text())} words found in the document")
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_path}")
+print("----------- Word Count ----------")
+print(f"Found {word_count(get_book_text())} total words")
+print("----------- Character Count -----------")
+#print(f"{char_count(get_book_text())}")
+for char in char_count_sorted(get_book_text()):
+    print(f"{char['name']}: {char['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -3,3 +3,22 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    char_dict = {}
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def char_count_sorted(text):
+    char_dict = char_count(text)
+    my_list = []
+
+    for key, value in char_dict.items():
+        if key.isalpha():
+            my_list.append({"name":key, "num":value})
+    return sorted(my_list, key=lambda x: x["num"], reverse=True)
+
