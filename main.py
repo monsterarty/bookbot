1037b626945fd5223c2be7fd9a5806a4939276a2
monsterarty <masleanco@gmail.com>
2025-04-23 13:18:14 +0200

    4: Added more books, loading them on start

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
+import sys
 from stats import word_count, char_count, char_count_sorted
 
-book_path = "./books/frankenstein.txt"
 
 
-def get_book_text():
 
-    with open(book_path, "r") as f:
-        file_content = f.read()
-        return file_content
-    
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at {book_path}")
-print("----------- Word Count ----------")
-print(f"Found {word_count(get_book_text())} total words")
-print("----------- Character Count -----------")
-#print(f"{char_count(get_book_text())}")
-for char in char_count_sorted(get_book_text()):
-    print(f"{char['name']}: {char['num']}")
-print("============= END ===============")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    def get_book_text():
+
+        with open(book_path, "r") as f:
+            file_content = f.read()
+            return file_content
+        
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count(get_book_text())} total words")
+    print("----------- Character Count -----------")
+    #print(f"{char_count(get_book_text())}")
+    for char in char_count_sorted(get_book_text()):
+        print(f"{char['name']}: {char['num']}")
+    print("============= END ===============")
+
+
+
+if __name__ == "__main__":
+    main()
